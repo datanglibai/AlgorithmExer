8476ef9f44fb460e4a7e1ffe673e09229cd17bc4
Yu WANG <yuzi_801212@sina.com.cn>
2016-11-24 00:07:32 +0800

    remove useless comments

--- com/company/Josephus.java
+++ com/company/Josephus.java
@@ -1,43 +1,42 @@
 package com.company;
 
-
 public class Josephus {
     CircularQueue q1 = new CircularQueue();
     CircularQueue q2 = new CircularQueue();
     Queue removedQ = new Queue();
     public void josephus(int[] data, int m)
     {
-        //init queue
-        for (int i =0 ; i < data.length; i++)
-        {
-            q1.enqueue(data[i]);
-        }
-
+        initQ(data);   //init q1
         int step = 1;
-        //System.out.println(!((q1.size() == 1 && q2.isEmpty()) || (q2.size() == 1 && q1.isEmpty())));
+
         while (!(q1.size() == 1 && q2.isEmpty())){
-            //if (q1.size() < m ) break;
             if(step == m)
             {
-                System.out.println(q1.dequeue());
-                //removedQ.enqueue(q1.dequeue());
+                System.out.println(q1.dequeue());      //removedQ.enqueue(q1.dequeue());
                 step = 1;
             }
             else
             {
                 q2.enqueue(q1.dequeue());
-                step ++;
+                step++;
             }
             if(q1.isEmpty())
             {
-                //exchange queue
                 CircularQueue temp = q1;
                 q1 = q2;
                 q2 = temp;
             }
         }
 
-        System.out.println(q1.dequeue());
+        System.out.println("the survival is: " + q1.dequeue());
 
+    }
+
+    private void initQ(int[] data)
+    {
+        for (int i =0 ; i < data.length; i++)
+        {
+            q1.enqueue(data[i]);
+        }
     }
 }
